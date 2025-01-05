38e13fff667bce262ad10111481668d3699bda0d
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-01-06 00:22:50 +0700

    feat: visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,17 +1,9 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-from dsatur_algorithm import dsatur_algorithm
-from insert_room_type import create_room_graph
 
 def visualize_colored_graph(graph, colors, m, n):
     """
     Fungsi untuk memvisualisasikan graf dengan simpul berwarna sesuai hasil pewarnaan.
-
-    Parameters:
-        graph (nx.Graph): Graf NetworkX.
-        colors (dict): Hasil pewarnaan simpul dalam format {simpul: warna}.
-        m (int): Jumlah baris pada graf persegi panjang.
-        n (int): Jumlah kolom pada graf persegi panjang.
     """
     # Posisi simpul dalam bentuk grid persegi panjang
     pos = {(x, y): (y, -x) for x, y in graph.nodes()}
@@ -31,6 +23,5 @@ def visualize_colored_graph(graph, colors, m, n):
         font_color='black',
         font_weight='bold',
     )
-    plt.title("Visualisasi Graf Berwarna (Persegi Panjang)", fontsize=16)
     plt.axis("equal")
     plt.show()
